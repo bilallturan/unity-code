45184d9bb8ec93353edea7d2477511d7b074a09c
bilallturan <65107216+bilallturan@users.noreply.github.com>
2020-11-24 17:52:33 +0300

    Add files via upload

---
+++ KameraHareket.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraHareket : MonoBehaviour
+{
+    public GameObject top;
+    Vector3 AradakiMesafe;   
+
+    void Start()
+    {
+        AradakiMesafe = transform.position - top.transform.position;
+    }
+
+    
+    void LateUpdate()
+    {
+        transform.position = top.transform.position + AradakiMesafe;
+    }
+}
---
+++ TopHareket.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopHareket : MonoBehaviour
+{
+    Rigidbody fizik;
+    public int hiz;
+
+    void Start ()
+    {
+        fizik = GetComponent<Rigidbody>();
+    }
+
+    
+    void FixedUpdate ()
+    {
+        float yatay = Input.GetAxisRaw("Horizontal");
+        // Horizontal yatay demektir
+        float dikey = Input.GetAxisRaw("Vertical");
+        // Vertical diket demektir
+
+        Vector3 vec = new Vector3(yatay,0,dikey);
+
+        fizik.AddForce(vec * hiz);
+    }
+}
